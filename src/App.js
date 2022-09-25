dad67c73e37486ddd47935ca862efd61af887e42
Siddhant <siddhantsharma669@gmail.com>
2022-09-26 01:12:37 +0530

    Done with the basic layout , created main routes , created neede pages and components

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+
+import './App.css'
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer'
+
+
+const App = () => {
+
+  return (
+
+    <Router>
+
+      <Box width="400px" sx={{ width: { xl : '1488px' }}} m ="auto" >
+
+        <Navbar />
+        <Routes >
+
+          <Route path="/" element={<Home />} />
+          <Route path="/excercise/:id" element={<ExerciseDetail />} />
+
+        </Routes>
+
+        <Footer />
+      </Box>
+
+    </Router>
+  )
+
+}
+
+export default App
